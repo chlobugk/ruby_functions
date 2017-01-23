8b7ff812805e79be7c74d8ff92f17ce574a8171b
Chloe Krainbucher <chloekrainbucher@Chloes-MacBook-Pro.local>
2017-01-23 11:01:30 -0500

    4 mult pass

--- test_multiply.rb
+++ test_multiply.rb
@@ -13,4 +13,8 @@ class TestMMFunctions < MiniTest::Test
 	def test_neg10_times_5_equals_neg50
 		assert_equal(-50, multiply(-10,5))
 	end
+
+	def test_10_times_0_equals_0
+		assert_equal(0, multiply(10,0))
+	end
 end
